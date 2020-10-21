6977c2b52a4b7e9a28f62f960b8bfb8f98ce56c7
khalidasm <khalid.asm.m@gmail.com>
2020-10-21 14:00:39 +0300

    update header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
 import { Switch ,Route } from 'react-router-dom'
 
 const HatsPage = () => (
@@ -12,6 +13,7 @@ const HatsPage = () => (
 function App() {
   return ( 
     <div>
+    <Header />
     <Switch>
     <Route exact path='/' component={HomePage}/>
     <Route exact path='/shop/hats' component={HatsPage}/>
