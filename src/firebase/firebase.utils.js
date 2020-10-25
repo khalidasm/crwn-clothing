56b70ea2181cb497d1f84ff9cca22cadbbfbcb8d
khalidasm <khalid.asm.m@gmail.com>
2020-10-25 16:20:01 +0300

    Sign up done

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,31 @@ const config = {
     appId: "1:1058408190100:web:25c91e69db109a77956481",
     measurementId: "G-3LK6TL03YY"
   };
+
+  export const CreateUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+  
+    const userRef = firestore.doc(`users/${userAuth.email}`);
+  
+    const snapShot = await userRef.get();
+  
+    if (!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        });
+      } catch (error) {
+        console.log('error creating user', error.message);
+      }
+    }
+  
+    return userRef;
+  };
   
   firebase.initializeApp(config);
   export const auth = firebase.auth();
@@ -20,5 +45,5 @@ const config = {
   const provider = new firebase.auth.GoogleAuthProvider();
   provider.setCustomParameters({ prompt: 'select_account' });
   export const signInWithGoogle = () => auth.signInWithPopup(provider);
-  export default firebase;
+  export default firebase; 
 
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './sign-in-and-sign-up.style.scss'
 import SignIn from '../../components/Sign-In/Sign-In.component'
+import SignUp from '../../components/sign-up/sign-up.component'
 
 
 const SignInAndSignUpPage = () => (
+    <div className='sign-in-sign-up'>
     <SignIn />
+    <SignUp />
+    </div>
 )
 
 export default SignInAndSignUpPage
